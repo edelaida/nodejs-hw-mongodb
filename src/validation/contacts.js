c4a37d0d3e4fd73d097da7bd43373986735b2631
edelaida <edelaida@gmail.com>
2024-12-01 15:42:48 +0200

    validate-modul

---
+++ src/validation/contacts.js
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.string().required(),
+  email: Joi.string().email().required(),
+  isFavourite: Joi.boolean().required(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),    
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.string(),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),    
+});
