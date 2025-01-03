67412c2ddfe15322dc451f68b82c7d0d9083e00f
edelaida <edelaida@gmail.com>
2025-01-03 14:55:14 +0200

    docs swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import {env} from './utils/env.js'
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { UPLOAD_DIR } from "./constants/index.js";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -18,6 +19,7 @@ export const setupServer = () => {
   
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
 app.use(
     pino({
